0d73fd8582cda0e1e54ef402e377bd8c4b37a599
AzureAD\KavishMojhoa-Zapproa <kavish@zapproach.com>
2026-04-18 14:06:55 +0400

    .

--- lib/kanban-notification-recipients.ts
+++ lib/kanban-notification-recipients.ts
@@ -17,4 +17,8 @@ export const KANBAN_NOTIFICATION_RECIPIENTS: KanbanNotificationRecipient[] = [
     email: "kavish17mojhoa+board@gmail.com",
     phone: "+23057003456",
   },
+  { id: "d-1", name: "Deloitte User 1", email: "luexternalahossany@deloitte.lu", phone: "+23057002345" },
+  
+  { id: "d-2", name: "Deloitte User 2", email: "kavish17mojhoa@gmail.com", phone: "+23057002345" },
+  
 ]
